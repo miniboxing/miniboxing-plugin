12a1879192c9eb296adc0c30fa4cbee5b5ed9ce1
Vlad Ureche <vlad.ureche@gmail.com>
2012-04-26 13:36:16 +0200

    More yak shaving :)

--- test/src/launch/Main.scala
+++ test/src/launch/Main.scala
@@ -2,11 +2,8 @@ package launch
 
 object Main {
   def main(args: Array[String]) {
-
-    val args = Array("test/Test.scala")
-
+    val args = Array("-Xplugin:lib/minibox.jar", "Test.scala")
     println("running")
-
     scala.tools.nsc.Main.main(args);
   }
 }
