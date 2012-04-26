e3af35923f984ccb81cad74d39542eb9bea3fca8
Cristian Tălău <cristitalau@gmail.com>
2012-04-26 17:01:11 +0200

    work with only one project

--- src/launch/Main.scala
+++ src/launch/Main.scala
@@ -2,7 +2,7 @@ package launch
 
 object Main {
   def main(args: Array[String]) {
-    val args = Array("-Xplugin:lib/minibox.jar", "Test.scala")
+    val args = Array("-Xplugin:lib/minibox.jar", "test/Test.scala")
     println("running")
     scala.tools.nsc.Main.main(args);
   }
