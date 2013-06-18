cb93da179772989e17b8c54555b9e9cebc2004dd
Vlad Ureche <vlad.ureche@gmail.com>
2013-06-19 01:18:47 +0200

    Tests for separate compilation

---
+++ tests/correctness/src/miniboxing/correctness/new/mb_arraybuffer_2.scala
@@ -0,0 +1,14 @@
+package miniboxing.benchmarks
+
+object Test2 extends App {
+  val a = new MBResizableArray[Int](implicitly[Manifest[Int]])
+  a.add(1)
+  a.add(2)
+  a.add(3)
+  a.add(4)
+  a.add(5)
+  a.add(6)
+  a.reverse
+  assert(a.contains(3))
+}
+
