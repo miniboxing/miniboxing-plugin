d84e7e11c1574ddb7373f4d9cd17f34c94ee7a62
Vlad Ureche <vlad.ureche@gmail.com>
2013-07-02 16:33:30 +0200

    Tests

---
+++ tests/correctness/src/miniboxing/tests/compile/mb_tparams_rewire_method.scala
@@ -0,0 +1,33 @@
+package miniboxing.tests.compile
+
+import miniboxing.plugin.minispec
+
+/**
+ * We can distinguish 5 cases:
+ * (1) selecting the method from the current object (this) ==>
+ *     we use the partial specialization info and type tags from the current method
+ * (2) selecting a method from a specialized interface with type parameters
+ *     2.1. fixed, specializable (Int, Long)
+ *     2.2. fixed, not specializable (String)
+ *     2.3. from the current scope, but not specialized
+ *     2.4. from the current scope, but specialized
+ */
+class TR3[@minispec T, Z] {
+  def foo[X, Y](t: T, x: X, y: Y): T = foo(t, y, x)        // case (1)
+  def bar[X, Y](t: TR3[T,Z], x: X, y: Y) = t.foo(???, x, y) // case (2.4)
+}
+
+class RT3[@minispec U, G](g: G) {
+  def test(u: U) = {
+    val tr = new TR3[U, Int]
+    tr.foo[G, Any](u, g, g)
+  }
+}
+
+object Test3 {
+  def test[T, A](a: A) = {
+    (new TR3[Int, String]).foo(3, a, a)                     // case (2.1)
+    (new TR3[String, Int]).foo("xxx", a, 4)                 // case (2.2)
+    (new TR3[T, Long]).foo[Int, Any](???, 2, 3)             // case (2.3)
+  }
+}
---
+++ tests/correctness/src/miniboxing/tests/compile/mb_tparams_rewire_new.scala
@@ -0,0 +1,26 @@
+package miniboxing.tests.compile.tparams
+
+import miniboxing.plugin.minispec
+
+/**
+ * We can distinguish 5 cases:
+ * (1) creating an object from the current object, thus inheriting its specs
+ * (2) creating an object from outside, with type params:
+ *     2.1. fixed, specializable (Int, Long)
+ *     2.2. fixed, not specializable (String)
+ *     2.3. from the current scope, but not specialized
+ *     2.4. from the current scope, but specialized => same as (1)
+ */
+class TN[@minispec T, X](val t: T){
+  def foo(): TN[T, String] =
+    new TN[T, String](t)           // case (1)
+}
+
+object TestNewRewire {
+  def test[T](t: T) = {
+    new TN[Int, String](1)         // case (2.1)
+    new TN[String, String]("xxx")  // case (2.2)
+    new TN[T, String](t)           // case (2.3)
+    new String("yyy")      // should not interfere
+  }
+}
