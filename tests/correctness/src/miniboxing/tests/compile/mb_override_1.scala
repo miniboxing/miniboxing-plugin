ec99761d9018932e54dde25d629d05c29bdc11f4
Vlad Ureche <vlad.ureche@gmail.com>
2013-07-13 02:00:47 +0200

    Test for first overriding example

---
+++ tests/correctness/src/miniboxing/tests/compile/mb_override_1.scala
@@ -0,0 +1,22 @@
+package miniboxing.tests.compile
+import miniboxing.plugin.minispec
+
+class Base1[@minispec T, @minispec S] {
+  def overrideMe(t: T, s: S): T = t
+}
+
+class Override11[T, S] extends Base1[T, S] {
+  override def overrideMe(t: T, s: S): T = ???
+}
+
+class Override12[T] extends Base1[T, Int] {
+  override def overrideMe(t: T, s: Int): T = ???
+}
+
+class Override13 extends Base1[Int, Int] {
+  override def overrideMe(t: Int, s: Int): Int = ???
+}
+
+// Other things to try:
+//  - overriding with minispec-annotated methods
+//  - overriding with normalized members
