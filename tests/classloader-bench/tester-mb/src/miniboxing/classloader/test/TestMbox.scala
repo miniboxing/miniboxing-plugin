32303885688acccaf07e2e8a095a4421665119f3
Vlad Ureche <vlad.ureche@epfl.ch>
2013-07-18 19:39:29 +0200

    In case classes are dumped

--- tests/classloader-bench/tester-mb/src/miniboxing/classloader/test/TestMbox.scala
+++ tests/classloader-bench/tester-mb/src/miniboxing/classloader/test/TestMbox.scala
@@ -8,6 +8,8 @@ object TestMboxing {
     var i = 0
     while (i < 9) {
       classloader.findClass("miniboxing.benchmarks.collection.immutable.Vector_class_" + i)
+      // if classes are dumped:
+      //Class.forName("miniboxing.benchmarks.collection.immutable.Vector_class_" + i)
       i += 1
     }
     time -= System.currentTimeMillis()
