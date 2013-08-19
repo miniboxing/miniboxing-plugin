1338e7fbb424d019721a9696ca4c2b27d204cc7d
Vlad Ureche <vlad.ureche@gmail.com>
2013-08-20 00:45:49 +0200

    Move collection benchmarks where they belong

--- tests/lib-bench/src/miniboxing/library/bench/MiniboxedHC.scala
+++
@@ -1,7 +0,0 @@
-package miniboxing.library.bench
-
-object MiniboxedHC {
-  /* TODO: Fill in with actual class */
-  type List[T] = scala.collection.immutable.List[T]
-  def getList(size: Int): List[Float] = (1 to size).map(_.toFloat).toList
-}
