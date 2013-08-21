93cc32648a1a35f949b91513d30c10c0133856b1
Vlad Ureche <vlad.ureche@gmail.com>
2013-08-22 01:28:22 +0200

    Java-based runtime, float and double stay separate

---
+++ tests/benchmarks/src/miniboxing/runtime/java/MiniboxArray.java
@@ -0,0 +1,206 @@
+package miniboxing.runtime.java;
+import miniboxing.runtime.java.MiniboxConstants;
+
+public class MiniboxArray {
+
+  final static Object unitArray = new Object();
+
+  final static <T> Object mbarray_new(int len, byte tag) {
+    if (tag >= MiniboxConstants.CHAR)
+      return mbarray_new_1(len, tag);
+    else
+      return mbarray_new_2(len, tag);
+  }
+
+  final static <T> Object mbarray_new_1(int len, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.BOOLEAN:
+        return new boolean[len];
+      case MiniboxConstants.BYTE:
+        return new byte[len];
+      case MiniboxConstants.SHORT:
+        return new short[len];
+    }
+    return unitArray;
+  }
+
+  final static <T> Object mbarray_new_2(int len, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.CHAR:
+        return new char[len];
+      case MiniboxConstants.INT:
+        return new int[len];
+      case MiniboxConstants.LONG:
+        return new long[len];
+    }
+    return unitArray;
+  }
+
+////////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  final static long mbarray_apply_minibox(Object array, int idx, byte tag) {
+    if (tag >= MiniboxConstants.CHAR)
+      return mbarray_apply_minibox_1(array, idx, tag);
+    else
+      return mbarray_apply_minibox_2(array, idx, tag);
+  }
+
+  final static long mbarray_apply_minibox_1(Object array, int idx, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.BOOLEAN:
+        if (((boolean[])array)[idx])
+          return 1;
+        else
+          return 0;
+      case MiniboxConstants.BYTE:
+        return ((byte[])array)[idx];
+      case MiniboxConstants.SHORT:
+        return ((short[])array)[idx];
+    }
+    return 0;
+  }
+
+  final static long mbarray_apply_minibox_2(Object array, int idx, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.CHAR:
+        return ((char[])array)[idx];
+      case MiniboxConstants.INT:
+        return ((int[])array)[idx];
+      case MiniboxConstants.LONG:
+        return ((long[])array)[idx];
+    }
+    return 0;
+  }
+
+////////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  final static <T> T mbarray_apply_box(Object array, int idx, byte tag) {
+    if (tag >= MiniboxConstants.CHAR)
+      return (T)mbarray_apply_box_1(array, idx, tag);
+    else
+      return (T)mbarray_apply_box_2(array, idx, tag);
+  }
+
+  final static Object mbarray_apply_box_1(Object array, int idx, byte tag) {
+    switch (tag) {
+    case MiniboxConstants.BOOLEAN:
+      return ((boolean[])array)[idx];
+    case MiniboxConstants.BYTE:
+      return ((byte[])array)[idx];
+    case MiniboxConstants.SHORT:
+      return ((short[])array)[idx];
+    }
+    return 0;
+  }
+
+  final static Object mbarray_apply_box_2(Object array, int idx, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.CHAR:
+        return ((char[])array)[idx];
+      case MiniboxConstants.INT:
+        return ((int[])array)[idx];
+      case MiniboxConstants.LONG:
+        return ((long[])array)[idx];
+    }
+    return 0;
+  }
+
+////////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  final static void mbarray_update_minibox(Object array, int idx, long value, byte tag) {
+    if (tag >= MiniboxConstants.CHAR)
+      mbarray_update_minibox_1(array, idx, value, tag);
+    else
+      mbarray_update_minibox_2(array, idx, value, tag);
+  }
+
+  final static void mbarray_update_minibox_1(Object array, int idx, long value, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.BOOLEAN:
+        if (value == 0)
+          ((boolean[])array)[idx] = false;
+        else
+          ((boolean[])array)[idx] = true;
+      case MiniboxConstants.BYTE:
+        ((byte[])array)[idx] = (byte)value;
+      case MiniboxConstants.SHORT:
+        ((short[])array)[idx] = (short)value;
+    }
+  }
+
+  final static void mbarray_update_minibox_2(Object array, int idx, long value, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.CHAR:
+        ((char[])array)[idx] = (char)value;
+      case MiniboxConstants.INT:
+        ((int[])array)[idx] = (int)value;
+      case MiniboxConstants.LONG:
+        ((long[])array)[idx] = value;
+    }
+  }
+
+////////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  final static <T> void mbarray_update_box(Object array, int idx, T value, byte tag) {
+    if (tag >= MiniboxConstants.CHAR)
+      mbarray_update_box_1(array, idx, value, tag);
+    else
+      mbarray_update_box_2(array, idx, value, tag);
+  }
+
+  final static void mbarray_update_box_1(Object array, int idx, Object value, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.BOOLEAN:
+        ((boolean[])array)[idx] = (java.lang.Boolean)value;
+      case MiniboxConstants.BYTE:
+        ((byte[])array)[idx] = (java.lang.Byte)value;
+      case MiniboxConstants.SHORT:
+        ((short[])array)[idx] = (java.lang.Short)value;
+    }
+  }
+
+  final static void mbarray_update_box_2(Object array, int idx, Object value, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.CHAR:
+        ((char[])array)[idx] = (java.lang.Character)value;
+      case MiniboxConstants.INT:
+        ((int[])array)[idx] = (java.lang.Integer)value;
+      case MiniboxConstants.LONG:
+        ((long[])array)[idx] = (java.lang.Long)value;
+    }
+  }
+
+////////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+  final static int mbarray_length(Object array, byte tag) {
+    if (tag >= MiniboxConstants.CHAR)
+      return mbarray_length_1(array, tag);
+    else
+      return mbarray_length_2(array, tag);
+  }
+
+  final static int mbarray_length_1(Object array, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.BOOLEAN:
+        return ((boolean[])array).length;
+      case MiniboxConstants.BYTE:
+        return ((byte[])array).length;
+      case MiniboxConstants.SHORT:
+        return ((short[])array).length;
+    }
+    return 0;
+  }
+
+  final static int mbarray_length_2(Object array, byte tag) {
+    switch (tag) {
+      case MiniboxConstants.CHAR:
+        return ((char[])array).length;
+      case MiniboxConstants.INT:
+        return ((int[])array).length;
+      case MiniboxConstants.LONG:
+        return ((long[])array).length;
+    }
+    return 0;
+  }
+}
+
