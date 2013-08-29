765903b86f6032cb67b3d8b5a9fe5ef4cc7b8f26
Vlad Ureche <vlad.ureche@gmail.com>
2013-08-29 17:27:16 +0200

    Checkpoint. I know what needs to be done

--- components/plugin/src/miniboxing/plugin/MiniboxSpecTreeTransformer.scala
+++ components/plugin/src/miniboxing/plugin/MiniboxSpecTreeTransformer.scala
@@ -15,14 +15,39 @@ trait MiniboxPostTreeTransformer extends TypingTransformers {
   import Flags._
   import typer.{ typed, atOwner }
 
-  override def newTransformer(unit: CompilationUnit): Transformer = new MiniboxTreeTransformer(unit)
+  override def newTransformer(unit: CompilationUnit): Transformer = new Transformer {
+    val specTrans = new MiniboxTreeTransformer(unit)
+    override def transform(tree: Tree): Tree =
+      afterMiniboxSpec(checkNoStorage(specTrans.transform(tree)))
+  }
+
+  def checkNoStorage(tree: Tree) = {
+    for (t <- tree)
+      assert(noStorageAnnot(t.tpe), t + ": " + t.tpe)
+    tree
+  }
+
+  def noStorageAnnot(t: Type): Boolean = {
+    var hasStorage = false
+    new TypeMap {
+      def apply(tp: Type): Type = mapOver(tp)
+      override def mapOver(tp: Type): Type = tp match {
+        case _ if tp hasAnnotation(StorageClass) =>
+          hasStorage = true
+          tp
+        case _ =>
+          super.mapOver(tp)
+      }
+    }.apply(t)
+
+    !hasStorage
+  }
 
   class MiniboxTreeTransformer(unit: CompilationUnit) extends TypingTransformer(unit) {
 
-    override def transform(tree: Tree): Tree = afterMiniboxSpec(checkNoStorage(specTransform(tree)))
+    override def transform(tree: Tree): Tree = specTransform(tree)
 
     def specTransform(tree: Tree): Tree = {
-//      println("specTransform(" + tree + ")")
       val oldTpe = tree.tpe
       if (tree.hasSymbol)
         tree.symbol.info // force new info on the symbol
@@ -30,42 +55,26 @@ trait MiniboxPostTreeTransformer extends TypingTransformers {
       tree match {
         case EmptyTree =>
           EmptyTree
+        case Apply(fun, args) =>
+          val nfun = specTransform(fun)
+          val nargs = args.map(specTransform)
+          println(tree)
+          println("after: " + fun.tpe)
+          println("args: " + args.map(_.tpe).mkString(", "))
+          // TODO: For the arguments, if the argument's type is Tsp @storage and the
+          // function's argument is Tsp, we should apply minibox2box to convert it to
+          // the miniboxed representation
+          localTyper.typed(Apply(nfun, args))
         case _ =>
           val tree1 = super.transform(tree)
           val oldTpe = tree.tpe
           val newTpe = deepTransformation(tree.tpe)
           tree.tpe = newTpe
           assert(noStorageAnnot(tree.tpe), tree + "   <old>: " + oldTpe + "   <new>: " + newTpe)
-//          if (oldTpe.hasAnnotation(StorageClass) && newTpe =:= LongTpe)
-//            println("Need box2minibox conversion at: " + tree1)
-//          else if (newTpe.hasAnnotation(StorageClass) && oldTpe =:= LongTpe)
-//            println("Need minibox2box conversion at: " + tree1)
-          println(tree1 + ": " + newTpe)
+          // TODO: If the current type is Tsp (not Tsp @storage), and Tsp should be miniboxed,
+          // we should use box2minibox to convert it to the miniboxed representation.
           tree1
       }
     }
-
-    def checkNoStorage(tree: Tree) = {
-      for (t <- tree)
-        assert(noStorageAnnot(t.tpe), t + ": " + t.tpe)
-      tree
-    }
-
-    def noStorageAnnot(t: Type): Boolean = {
-      var hasStorage = false
-      new TypeMap {
-        def apply(tp: Type): Type = mapOver(tp)
-        override def mapOver(tp: Type): Type = tp match {
-          case _ if tp hasAnnotation(StorageClass) =>
-            hasStorage = true
-            tp
-          case _ =>
-            super.mapOver(tp)
-        }
-      }.apply(t)
-
-      !hasStorage
-    }
-
   }
 }
