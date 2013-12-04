708b5d08bf35157905ce8a3a7747c914ad338f86
Vlad Ureche <vlad.ureche@gmail.com>
2013-12-04 01:06:06 +0100

    First iteration of adaptation
    Doesn't recurse deep in the tree

--- components/plugin/src/miniboxing/plugin/MiniboxAdaptTreeTransformer.scala
+++ components/plugin/src/miniboxing/plugin/MiniboxAdaptTreeTransformer.scala
@@ -1,21 +1,46 @@
 package miniboxing.plugin
 
-import scala.reflect.internal.Flags
-import scala.tools.nsc.transform.TypingTransformers
-import scala.collection.mutable.Set
+import scala.tools.nsc._
 import scala.tools.nsc.typechecker._
+import scala.tools.nsc.symtab.Flags
+import scala.tools.nsc.transform.TypingTransformers
+import scala.collection.{ mutable, immutable }
+import scala.collection.mutable.Set
 import scala.collection.mutable.{ Map => MMap }
 
 trait MiniboxAdaptTreeTransformer extends TypingTransformers {
   self: MiniboxAdaptComponent =>
 
   import global._
-//  import definitions._
-//  import minibox._
-//  import Flags._
-//  import typer.{ typed, atOwner }
 
-  def newTransformer(unit: CompilationUnit): Transformer = new Transformer {
-    override def transform(tree: Tree): Tree = tree
+  object TreeAdapter extends {
+    val global: MiniboxAdaptTreeTransformer.this.global.type = MiniboxAdaptTreeTransformer.this.global
+  } with TreeAdapter
+
+  def newTransformer(unit: CompilationUnit): Transformer = new TypingTransformer(unit) {
+    override def transform(tree: Tree): Tree =
+      TreeAdapter.adapted(localTyper.context1.asInstanceOf[TreeAdapter.Context], tree)
+  }
+
+  abstract class TreeAdapter extends Analyzer {
+    import global._
+    import definitions.{ AnyRefClass, AnyValClass }
+    val StorageClass = minibox.StorageClass.asInstanceOf[Symbol]
+
+    def adapted(context: Context, tree: Tree): Tree =
+      (new Adapter(context)).typed(tree)
+
+    class Adapter(_context: Context) extends Typer(_context) {
+      override def typed(tree: Tree, mode: Int, pt: Type): Tree = {
+        val oldTpe = tree.tpe
+        tree.tpe = null
+        super.typed(tree, mode, WildcardType)
+        //if (tree.tpe.hasAnnotation(StorageClass) != oldTpe.hasAnnotation(StorageClass))
+        println(tree)
+        println("new = " + tree.tpe + " vs old: " + oldTpe)
+        tree
+      }
+    }
   }
 }
+
