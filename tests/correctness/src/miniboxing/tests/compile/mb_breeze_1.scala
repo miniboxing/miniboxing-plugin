d54c5ff0e9b58803d2df48c5dc9e67c10b7e5492
Vlad Ureche <vlad.ureche@gmail.com>
2014-01-14 13:25:42 +0100

    Ported a breeze bug (#35)

--- tests/correctness/src/miniboxing/tests/compile/mb_breeze_1.scala
+++ tests/correctness/src/miniboxing/tests/compile/mb_breeze_1.scala
@@ -1,5 +1,6 @@
-package miniboxing.tests.compile
+package miniboxing.tests.compile.bug35
 
+// https://github.com/miniboxing/miniboxing-plugin/issues/35
 
 trait TensorLike[@miniboxed K, @miniboxed V, +This]
 trait VectorLike[@miniboxed E, +Self <: Vector[E]] extends TensorLike[Int, E, Self]
