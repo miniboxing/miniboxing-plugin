16a21bca69653aa589df1763262e0edd923b756e
Vlad Ureche <vlad.ureche@gmail.com>
2015-04-29 16:19:38 +0200

    Changes to MbArrays

--- components/runtime/src/miniboxing/runtime/array/MbArrayImpls.scala
+++ components/runtime/src/miniboxing/runtime/array/MbArrayImpls.scala
@@ -31,30 +31,222 @@ class MbArray_L[T](private[array] final val array: Array[AnyRef]) extends MbArra
 
 }
 
-class MbArray_J[T](T_Tag: Byte, private[array] final val array: Array[Long]) extends MbArray[T] {
+class MbArray_I[T](private[array] final val array: Array[Int]) extends MbArray[T] {
 
-  def this(T_Tag: Byte, array: Array[T]) =
-    this(T_Tag, {
-      val array2 = new Array[Long](array.length)
+  def this(array: Array[T]) =
+    this({
+      val array2 = new Array[Int](array.length)
       var index = 0
       while (index < array.length) {
-        array2(index) = MiniboxConversionsLong.box2minibox_tt(array(index), T_Tag)
+        array2(index) = array(index).asInstanceOf[Int]
         index += 1
       }
       array2
     })
 
-  def this(T_Tag: Byte, size: Int) =
-    this(T_Tag, new Array[Long](size))
+  def this(size: Int) =
+    this(new Array[Int](size))
 
-  def apply(idx: Int): T = MiniboxConversionsLong.minibox2box(array(idx), T_Tag)
-  def update(idx: Int, value: T): Unit = array(idx) = MiniboxConversionsLong.box2minibox_tt(value, T_Tag)
-  override def clone: MbArray[T] = new MbArray_J[T](T_Tag, array.clone())
+  def apply(idx: Int): T = array(idx).asInstanceOf[T]
+  def update(idx: Int, value: T): Unit = array(idx) = value.asInstanceOf[Int]
+  override def clone: MbArray[T] = new MbArray_I[T](array.clone())
   def length: Int = array.length
 
   // optimized accessors:
-  def apply_J(idx: Int): Long = array(idx)
-  def update_J(idx: Int, value: Long): Unit = array(idx) = value
+  def apply_J(idx: Int): Long = MiniboxConversions.int2minibox(array(idx))
+  def update_J(idx: Int, value: Long): Unit = array(idx) = MiniboxConversions.minibox2int(value)
+  override def arraycopy(srcPos: Int, dest: MbArray[T], destPos: Int, length: Int) = {
+    dest match {
+      case dest_J: MbArray_I[T] => System.arraycopy(array, srcPos, dest_J.array, destPos, length)
+      case _ => super.arraycopy(srcPos, dest, destPos, length)
+    }
+  }
+}
+
+class MbArray_S[T](private[array] final val array: Array[Short]) extends MbArray[T] {
+
+  def this(array: Array[T]) =
+    this({
+      val array2 = new Array[Short](array.length)
+      var index = 0
+      while (index < array.length) {
+        array2(index) = array(index).asInstanceOf[Short]
+        index += 1
+      }
+      array2
+    })
+
+  def this(size: Int) =
+    this(new Array[Short](size))
+
+  def apply(idx: Int): T = array(idx).asInstanceOf[T]
+  def update(idx: Int, value: T): Unit = array(idx) = value.asInstanceOf[Short]
+  override def clone: MbArray[T] = new MbArray_S[T](array.clone())
+  def length: Int = array.length
+
+  // optimized accessors:
+  def apply_J(idx: Int): Long = MiniboxConversions.short2minibox(array(idx))
+  def update_J(idx: Int, value: Long): Unit = array(idx) = MiniboxConversions.minibox2short(value)
+  override def arraycopy(srcPos: Int, dest: MbArray[T], destPos: Int, length: Int) = {
+    dest match {
+      case dest_J: MbArray_S[T] => System.arraycopy(array, srcPos, dest_J.array, destPos, length)
+      case _ => super.arraycopy(srcPos, dest, destPos, length)
+    }
+  }
+}
+
+class MbArray_B[T](private[array] final val array: Array[Byte]) extends MbArray[T] {
+
+  def this(array: Array[T]) =
+    this({
+      val array2 = new Array[Byte](array.length)
+      var index = 0
+      while (index < array.length) {
+        array2(index) = array(index).asInstanceOf[Byte]
+        index += 1
+      }
+      array2
+    })
+
+  def this(size: Int) =
+    this(new Array[Byte](size))
+
+  def apply(idx: Int): T = array(idx).asInstanceOf[T]
+  def update(idx: Int, value: T): Unit = array(idx) = value.asInstanceOf[Byte]
+  override def clone: MbArray[T] = new MbArray_B[T](array.clone())
+  def length: Int = array.length
+
+  // optimized accessors:
+  def apply_J(idx: Int): Long = MiniboxConversions.byte2minibox(array(idx))
+  def update_J(idx: Int, value: Long): Unit = array(idx) = MiniboxConversions.minibox2byte(value)
+  override def arraycopy(srcPos: Int, dest: MbArray[T], destPos: Int, length: Int) = {
+    dest match {
+      case dest_J: MbArray_B[T] => System.arraycopy(array, srcPos, dest_J.array, destPos, length)
+      case _ => super.arraycopy(srcPos, dest, destPos, length)
+    }
+  }
+}
+
+class MbArray_Z[T](private[array] final val array: Array[Boolean]) extends MbArray[T] {
+
+  def this(array: Array[T]) =
+    this({
+      val array2 = new Array[Boolean](array.length)
+      var index = 0
+      while (index < array.length) {
+        array2(index) = array(index).asInstanceOf[Boolean]
+        index += 1
+      }
+      array2
+    })
+
+  def this(size: Int) =
+    this(new Array[Boolean](size))
+
+  def apply(idx: Int): T = array(idx).asInstanceOf[T]
+  def update(idx: Int, value: T): Unit = array(idx) = value.asInstanceOf[Boolean]
+  override def clone: MbArray[T] = new MbArray_Z[T](array.clone())
+  def length: Int = array.length
+
+  // optimized accessors:
+  def apply_J(idx: Int): Long = MiniboxConversions.boolean2minibox(array(idx))
+  def update_J(idx: Int, value: Long): Unit = array(idx) = MiniboxConversions.minibox2boolean(value)
+  override def arraycopy(srcPos: Int, dest: MbArray[T], destPos: Int, length: Int) = {
+    dest match {
+      case dest_J: MbArray_Z[T] => System.arraycopy(array, srcPos, dest_J.array, destPos, length)
+      case _ => super.arraycopy(srcPos, dest, destPos, length)
+    }
+  }
+}
+
+class MbArray_C[T](private[array] final val array: Array[Char]) extends MbArray[T] {
+
+  def this(array: Array[T]) =
+    this({
+      val array2 = new Array[Char](array.length)
+      var index = 0
+      while (index < array.length) {
+        array2(index) = array(index).asInstanceOf[Char]
+        index += 1
+      }
+      array2
+    })
+
+  def this(size: Int) =
+    this(new Array[Char](size))
+
+  def apply(idx: Int): T = array(idx).asInstanceOf[T]
+  def update(idx: Int, value: T): Unit = array(idx) = value.asInstanceOf[Char]
+  override def clone: MbArray[T] = new MbArray_C[T](array.clone())
+  def length: Int = array.length
+
+  // optimized accessors:
+  def apply_J(idx: Int): Long = MiniboxConversions.char2minibox(array(idx))
+  def update_J(idx: Int, value: Long): Unit = array(idx) = MiniboxConversions.minibox2char(value)
+  override def arraycopy(srcPos: Int, dest: MbArray[T], destPos: Int, length: Int) = {
+    dest match {
+      case dest_J: MbArray_C[T] => System.arraycopy(array, srcPos, dest_J.array, destPos, length)
+      case _ => super.arraycopy(srcPos, dest, destPos, length)
+    }
+  }
+}
+
+class MbArray_V[T](private[array] final val array: Array[Unit]) extends MbArray[T] {
+
+  def this(array: Array[T]) =
+    this({
+      val array2 = new Array[Unit](array.length)
+      var index = 0
+      while (index < array.length) {
+        array2(index) = array(index).asInstanceOf[Unit]
+        index += 1
+      }
+      array2
+    })
+
+  def this(size: Int) =
+    this(new Array[Unit](size))
+
+  def apply(idx: Int): T = array(idx).asInstanceOf[T]
+  def update(idx: Int, value: T): Unit = array(idx) = value.asInstanceOf[Unit]
+  override def clone: MbArray[T] = new MbArray_V[T](array.clone())
+  def length: Int = array.length
+
+  // optimized accessors:
+  def apply_J(idx: Int): Long = 0 // TODO: WTF
+  def update_J(idx: Int, value: Long): Unit = array(idx) = Unit // TODO: WTF
+  override def arraycopy(srcPos: Int, dest: MbArray[T], destPos: Int, length: Int) = {
+    dest match {
+      case dest_J: MbArray_V[T] => System.arraycopy(array, srcPos, dest_J.array, destPos, length)
+      case _ => super.arraycopy(srcPos, dest, destPos, length)
+    }
+  }
+}
+
+class MbArray_J[T](private[array] final val array: Array[Long]) extends MbArray[T] {
+
+  def this(array: Array[T]) =
+    this({
+      val array2 = new Array[Long](array.length)
+      var index = 0
+      while (index < array.length) {
+        array2(index) = array(index).asInstanceOf[Long]
+        index += 1
+      }
+      array2
+    })
+
+  def this(size: Int) =
+    this(new Array[Long](size))
+
+  def apply(idx: Int): T = array(idx).asInstanceOf[T]
+  def update(idx: Int, value: T): Unit = array(idx) = value.asInstanceOf[Long]
+  override def clone: MbArray[T] = new MbArray_J[T](array.clone())
+  def length: Int = array.length
+
+  // optimized accessors:
+  def apply_J(idx: Int): Long = MiniboxConversions.long2minibox(array(idx))
+  def update_J(idx: Int, value: Long): Unit = array(idx) = MiniboxConversions.minibox2long(value)
   override def arraycopy(srcPos: Int, dest: MbArray[T], destPos: Int, length: Int) = {
     dest match {
       case dest_J: MbArray_J[T] => System.arraycopy(array, srcPos, dest_J.array, destPos, length)
@@ -62,6 +254,7 @@ class MbArray_J[T](T_Tag: Byte, private[array] final val array: Array[Long]) ext
     }
   }
 }
+
 
 class MbArray_D[T](T_Tag: Byte, private[array] final val array: Array[Double]) extends MbArray[T] {
 
--- components/runtime/src/miniboxing/runtime/array/MbArrayOpts.java
+++ components/runtime/src/miniboxing/runtime/array/MbArrayOpts.java
@@ -1,15 +1,30 @@
 package miniboxing.runtime.array;
 
 import miniboxing.runtime.*;
+import miniboxing.runtime.MiniboxConstants;
 import scala.MbArray;
 
 public class MbArrayOpts {
 
-  public static <T> long mbArray_apply_J(MbArray<T> mbArray, int index, byte T_Tag) {
-    if (mbArray instanceof MbArray_J<?>)
+  public static final <T> long mbArray_apply_J(MbArray<T> mbArray, int index, byte T_Tag) {
+    switch(T_Tag) {
+    case MiniboxConstants.LONG:
       return ((MbArray_J<?>)mbArray).apply_J(index);
-    else
-      return MiniboxConversionsLong.box2minibox_tt(mbArray.apply(index), T_Tag);
+    case MiniboxConstants.INT:
+      return ((MbArray_I<?>)mbArray).apply_J(index);
+    case MiniboxConstants.SHORT:
+      return ((MbArray_S<?>)mbArray).apply_J(index);
+    case MiniboxConstants.CHAR:
+      return ((MbArray_C<?>)mbArray).apply_J(index);
+    case MiniboxConstants.BYTE:
+      return ((MbArray_B<?>)mbArray).apply_J(index);
+    case MiniboxConstants.BOOLEAN:
+      return ((MbArray_Z<?>)mbArray).apply_J(index);
+    case MiniboxConstants.UNIT:
+      return ((MbArray_V<?>)mbArray).apply_J(index);
+    default:
+      return MiniboxConversionsLong.<T>box2minibox_tt(mbArray.apply(index), T_Tag);
+    }
   }
 
   public static <T> double mbArray_apply_D(MbArray<T> mbArray, int index, byte T_Tag) {
@@ -19,11 +34,33 @@ public class MbArrayOpts {
       return MiniboxConversionsDouble.box2minibox_tt(mbArray.apply(index), T_Tag);
   }
 
-  public static <T> void mbArray_update_J(MbArray<T> mbArray, int index, long value, byte T_Tag) {
-    if (mbArray instanceof MbArray_J<?>)
-      ((MbArray_J<?>)mbArray).update_J(index, value);
-    else
-      mbArray.update(index, MiniboxConversionsLong.<T>minibox2box(value, T_Tag));
+  public static final <T> void mbArray_update_J(MbArray<T> mbArray, int index, long value, byte T_Tag) {
+    switch(T_Tag) {
+      case MiniboxConstants.LONG:
+        ((MbArray_J<?>)mbArray).update_J(index, value);
+        break;
+      case MiniboxConstants.INT:
+        ((MbArray_I<?>)mbArray).update_J(index, value);
+        break;
+      case MiniboxConstants.SHORT:
+        ((MbArray_S<?>)mbArray).update_J(index, value);
+        break;
+      case MiniboxConstants.CHAR:
+        ((MbArray_C<?>)mbArray).update_J(index, value);
+        break;
+      case MiniboxConstants.BYTE:
+        ((MbArray_B<?>)mbArray).update_J(index, value);
+        break;
+      case MiniboxConstants.BOOLEAN:
+        ((MbArray_Z<?>)mbArray).update_J(index, value);
+        break;
+      case MiniboxConstants.UNIT:
+        ((MbArray_V<?>)mbArray).update_J(index, value);
+        break;
+      default:
+        mbArray.update(index, MiniboxConversionsLong.<T>minibox2box(value, T_Tag));
+        break;
+    }
   }
 
   public static <T> void mbArray_update_D(MbArray<T> mbArray, int index, double value, byte T_Tag) {
@@ -33,18 +70,56 @@ public class MbArrayOpts {
       mbArray.update(index, MiniboxConversionsDouble.<T>minibox2box(value, T_Tag));
   }
 
-  public static <T> MbArray<T> mbArray_empty_J(int size, byte T_Tag) {
-    return new MbArray_J<T>(T_Tag, size);
+  @SuppressWarnings("unchecked")
+  public static final <T> MbArray<T> mbArray_empty_J(int size, byte T_Tag) {
+    switch(T_Tag) {
+    case MiniboxConstants.LONG:
+      return new MbArray_J<T>(size);
+    case MiniboxConstants.INT:
+      return new MbArray_I<T>(size);
+    case MiniboxConstants.SHORT:
+      return new MbArray_S<T>(size);
+    case MiniboxConstants.CHAR:
+      return new MbArray_C<T>(size);
+    case MiniboxConstants.BYTE:
+      return new MbArray_B<T>(size);
+    case MiniboxConstants.BOOLEAN:
+      return new MbArray_Z<T>(size);
+    case MiniboxConstants.UNIT:
+      return new MbArray_V<T>(size);
+    default:
+      return new MbArray_L<T>(size);
+    }
   }
 
+  @SuppressWarnings("unchecked")
   public static <T> MbArray<T> mbArray_empty_D(int size, byte T_Tag) {
     return new MbArray_D<T>(T_Tag, size);
   }
 
-  public static <T> MbArray<T> mbArray_clone_J(Object array, byte T_Tag) {
-    return new MbArray_J<T>(T_Tag, array);
+  @SuppressWarnings("unchecked")
+  public static final <T> MbArray<T> mbArray_clone_J(Object array, byte T_Tag) {
+    switch(T_Tag) {
+    case MiniboxConstants.LONG:
+      return new MbArray_J<T>(array);
+    case MiniboxConstants.INT:
+      return new MbArray_I<T>(array);
+    case MiniboxConstants.SHORT:
+      return new MbArray_S<T>(array);
+    case MiniboxConstants.CHAR:
+      return new MbArray_C<T>(array);
+    case MiniboxConstants.BYTE:
+      return new MbArray_B<T>(array);
+    case MiniboxConstants.BOOLEAN:
+      return new MbArray_Z<T>(array);
+    case MiniboxConstants.UNIT:
+      return new MbArray_V<T>(array);
+    default:
+      return new MbArray_L<T>(array);
+    }
   }
 
+  @SuppressWarnings("unchecked")
   public static <T> MbArray<T> mbArray_clone_D(Object array, byte T_Tag) {
     return new MbArray_D<T>(T_Tag, array);
   }
